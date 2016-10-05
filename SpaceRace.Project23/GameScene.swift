e86872e9dc873b9d798df3e2732635957bc43ba7
ElijahButers <Elijahbuters@gmail.com>
2016-10-05 13:42:35 +0300

    Added code to didMove(to view) in GameScene that creates a score label on the view, its font, its position, alignment mode.

--- SpaceRace.Project23/GameScene.swift
+++ SpaceRace.Project23/GameScene.swift
@@ -36,6 +36,11 @@ class GameScene: SKScene {
         player.physicsBody = SKPhysicsBody(texture: player.texture!, size: player.size)
         player.physicsBody!.contactTestBitMask = 1
         addChild(player)
+        
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.position = CGPoint(x: 16, y: 16)
+        scoreLabel.horizontalAlignmentMode = .left
+        addChild(scoreLabel)
     }
 }
 
